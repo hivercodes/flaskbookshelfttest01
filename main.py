f3bc3c85da09787d86d16345ba8056ae09a70a6b
Lars <lars.eriksson.mail@gmail.com>
2022-05-05 18:45:41 +0200

    fixed the example code.

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    if len(all_books) > 0:
+        return render_template("index.html", all_books=all_books)
+    else:
+        return render_template("indexempty.html")
 
 
-@app.route("/add")
+@app.route("/add", methods=['POST', 'GET'])
 def add():
-    pass
+    if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"],
+        }
+        all_books.append(new_book)
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
